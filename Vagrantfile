dc138e7085adacfcd274290f570df71e07cd8c21
Tobias Johansson <tobias@johansson.xyz>
2017-08-31 07:48:48 +0200

    move all configs to create_vm()

--- Vagrantfile
+++ Vagrantfile
@@ -5,23 +5,12 @@ require_relative "vmfactory"
 
 Vagrant.configure("2") do |config|
   spec = YAML.load_file("cluster-spec.yml")
-  config.vm.box = spec["cluster"]["default"]["box"]
-  config.vm.box_url = spec["cluster"]["default"]["box_url"]
   groups = {}
   host_vars = {}
   spec["cluster"]["groups"].each do |group, _|
     groups[group] = []
-    (0..spec["cluster"]["groups"][group]["nodes"]-1).each do |i|
-      hostname, ip = create_vm(config, spec, group, i)
-      groups[group].push(hostname)
-      host_vars[hostname] = {}
-      host_vars[hostname]["ip"] = ip
+    (0..spec["cluster"]["groups"][group]["nodes"]-1).each do |id|
+      create_vm(config, spec, groups, host_vars, group, id)
     end
-  end
-  config.vm.provision "ansible" do |ansible|
-    ansible.limit = "all"
-    ansible.playbook = "ansible/dummy.yml"
-    ansible.groups = groups
-    ansible.host_vars = host_vars
   end
 end
--- vmfactory.rb
+++ vmfactory.rb
@@ -1,8 +1,13 @@
-def create_vm(config, spec, group, id)
+def create_vm(config, spec, groups, host_vars, group, id)
   hostname = "%s%d" % [group, id]
   offset = spec["cluster"]["groups"][group]["offset"]
   ip = spec["cluster"]["network"]["ip"] + "#{offset+id}"
+  groups[group].push(hostname)
+  host_vars[hostname] = {}
+  host_vars[hostname]["ip"] = ip
   config.vm.define hostname do |vm|
+    vm.vm.box = spec["cluster"]["default"]["box"]
+    vm.vm.box_url = spec["cluster"]["default"]["box_url"]
     vm.vm.hostname = hostname
     vm.vm.network "private_network", ip: ip
     vm.vm.provider "virtualbox" do |vb|
@@ -10,6 +15,11 @@ def create_vm(config, spec, group, id)
       vb.cpus = resources["cpus"]
       vb.memory = resources["memory"]
     end
+    vm.vm.provision "ansible" do |ansible|
+      ansible.limit = "all"
+      ansible.playbook = "ansible/dummy.yml"
+      ansible.groups = groups
+      ansible.host_vars = host_vars
+    end
   end
-  return hostname, ip
 end
